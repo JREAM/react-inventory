2eb986fb2ab8de19812a26734307753950f58849
Jesse Boyer <imboyus@gmail.com>
2022-05-16 06:03:03 -0400

    notes

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -11,6 +11,8 @@ export default function Home() {
         <li><b>Todo</b></li>
         <li>Work on a possible real API rather than dummy data, eg: Contentful</li>
         <li>shop/:id where ID not found needs to show error not empty item.</li>
+        <li>SignIn/SignUp no access if HAS localstorage token</li>
+        <li>SignOut no access if NOT HAS localstorage token</li>
         <li>State needs to be triggered Navigation bar for Login/Logout, currently requires refresh. Redux maybe, but I want to use redux for more things rather than that one item if I use it.</li>
       </ul>
     </div>
